309486f3c1a5b04be47be42a704d4346506e05a0
KhahrahkDev <kaizerverdeentrata@gmail.com>
2026-09-10 21:08:15 +0800

    Add files via upload

---
+++ PersonalIntroduction.kt
@@ -0,0 +1,11 @@
+fun main() {
+    print("Enter your name: ")
+    var name = readln()
+    print("Enter your age: ")
+    var age = readln().toInt()
+    print("Enter your course: ")
+    var course = readln()
+    println()
+    println("Hello, $name!")
+    println("You are $age years old and taking $course")
+}
