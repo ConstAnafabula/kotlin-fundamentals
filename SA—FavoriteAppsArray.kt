72f4eb3deb69ed2f17a679f1c45ba388605802d1
KhahrahkDev <kaizerverdeentrata@gmail.com>
2026-09-17 19:58:20 +0800

    Add files via upload

---
+++ SA—FavoriteAppsArray.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var fav: Array<String> = arrayOf("Facebook", "Messenger", "YouTube", "TikTok", "Spotifiy")
+
+    println("Favorite Apps: ")
+    for (i in fav.indices) {
+        println(fav[i])
+    }
+}
