3ef9273c60079bdadc8ee14718b39975e3a62e92
KhahrahkDev <kaizerverdeentrata@gmail.com>
2026-09-20 14:22:01 +0800

    Add files via upload

---
+++ SA—GroceryPriceCalculator.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var arr: Array<Int> = arrayOf(23, 21, 12, 321, 123)
+
+    println("Total Price: " + arr.sum())
+    println("Average Price: " + arr.average())
+    println("Highest Price: " + arr.maxOrNull())
+}
